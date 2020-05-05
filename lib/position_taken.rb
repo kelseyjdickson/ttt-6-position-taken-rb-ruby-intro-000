04eb6a52af300ccff7a9a5cee63b1617c1e7506e
Kelsey Dickson <kelseymedved@gmail.com>
2020-05-05 17:53:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index] == " " and board[index] == ""
+  if board[index] == " " || board[index] == ""
     return false
 end
